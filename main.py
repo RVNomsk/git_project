03f45a1627b85fb0d8873d88f80c20200e15bd8d
rvn <rvnomsk@mail.ru>
2024-02-04 21:53:22 +0300

    Выгрузили БД из файла excel в sqlite3. Далее нужно создать файл main.ui Файл main.ui создан, использовали только TableWidget Создал main.py, он просто выгружает все данные БД в TableWidget

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-# This is a sample Python script.
+import sqlite3
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
 
 
-# Press the green button in the gutter to run the script.
+class CoffeeBase(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('main.ui', self)
+        self.connection = sqlite3.connect('data/coffee.sqlite')
+        # выведем содержимое базы данных в файл без всяких лишних действий
+        self.show_data()
+
+    def show_data(self):
+        query = "SELECT * FROM coffee"
+        res = self.connection.cursor().execute(query).fetchall()
+        self.tableWidget.setColumnCount(7)
+        self.tableWidget.setRowCount(0)
+        for i, row in enumerate(res):
+            self.tableWidget.setRowCount(self.tableWidget.rowCount() + 1)
+            for j, elem in enumerate(row):
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(elem)))
+
+    def closeEvent(self, event):
+        self.connection.close()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app = QApplication(sys.argv)
+    coffee_db = CoffeeBase()
+    coffee_db.show()
+    sys.exit(app.exec())
